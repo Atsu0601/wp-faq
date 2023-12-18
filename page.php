e46ef618c82bb8543cd50788f5c7dc5ab6f24754
Kawatani Atsushi <katsushi61@gmail.com>
2023-12-19 08:27:52 +0900

    CSS調整

--- page.php
+++ page.php
@@ -8,7 +8,9 @@
       <div class="p-page">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <h1 class="p-page__ttl"><?php the_title() ?></h1>
-            <?php the_content() ?>
+            <div class="p-page__content">
+              <?php the_content() ?>
+            </div>
         <?php endwhile;
         endif; ?>
       </div>
