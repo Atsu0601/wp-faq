f0765cc8cb21ca39fae3c57de09ad82309c68398
Kawatani Atsushi <katsushi61@gmail.com>
2023-12-24 18:07:30 +0900

    検索結果ページの調整

--- search.php
+++ search.php
@@ -5,20 +5,20 @@
         <?php get_template_part('temple-parts/breadcrumbs'); ?>
 
         <div class="l-column2">
-            <div class="p-search">
+            <div class="p-archive">
 
                 <?php if (have_posts()) : ?>
                     <?php if (isset($_GET['s']) && empty($_GET['s'])) : ?>
-                        <p>検索キーワードが未入力です。</p>
+                        <p class="p-search__result_ttl">検索キーワードが未入力です。</p>
                     <?php else : ?>
-                        <p>“<?php echo $_GET['s']; ?>”の検索結果：<?php echo $wp_query->found_posts; ?>件</p>
+                        <p class="p-search__result_ttl">“<?php echo $_GET['s']; ?>”の検索結果：<?php echo $wp_query->found_posts; ?>件</p>
                     <?php endif; ?>
 
-                    <ul class="p-search__blk">
+                    <ul class="p-archive__blk">
                         <?php while (have_posts()) : the_post(); ?>
-                            <li class="p-search__blk__list">
+                            <li class="p-archive__blk__list">
                                 <a href="<?php the_permalink(); ?>">
-                                    <div class="p-search__blk__list__thumbnail">
+                                    <div class="p-archive__blk__list__thumbnail">
                                         <?php
                                         // サムネイルが設定されているかを確認
                                         if (has_post_thumbnail()) {
@@ -32,7 +32,7 @@
                                         }
                                         ?>
                                     </div>
-                                    <h3 class="p-search__blk__list__ttl">
+                                    <h3 class="p-archive__blk__list__ttl">
                                         <?php
                                         if (mb_strlen($post->post_title, 'UTF-8') > 20) {
                                             $title = mb_substr($post->post_title, 0, 20, 'UTF-8');
@@ -42,7 +42,7 @@
                                         }
                                         ?>
                                     </h3>
-                                    <time class="p-search__blk__list__date"><?php the_time("Y/m/d"); ?></time>
+                                    <time class="p-archive__blk__list__date"><?php the_time("Y/m/d"); ?></time>
                                     <!-- <div class="">
                                     <?php
                                     $terms = get_the_terms($post->ID, 'blog-cat');
@@ -51,7 +51,7 @@
                                     }
                                     ?>
                                 </div> -->
-                                    <p class="p-search__blk__list__txt">
+                                    <p class="p-archive__blk__list__txt">
                                         <?php
                                         if (mb_strlen($post->post_content, 'UTF-8') > 55) {
                                             $content = str_replace('\n', '', mb_substr(strip_tags($post->post_content), 0, 55, 'UTF-8'));
