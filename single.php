05c6c729860e090147e71fbf43ca6c463fb91119
Kawatani Atsushi <katsushi61@gmail.com>
2023-12-28 08:29:14 +0900

    詳細ページに次へ前へのリンク追加

--- single.php
+++ single.php
@@ -15,10 +15,34 @@
                     <p>お探しの記事は見つかりませんでした。<br />
                     <?php endif; ?>
                     
-                    <div class="single_bottom_nav">
-                        <p class="<?php if (get_previous_post()) : ?>single_bottom_nav_prev<?php endif; ?>"><?php previous_post_link('%link', '前へ'); ?></p>
-                        <p class="single_bottom_nav_back"><a href="<?php echo get_post_type_archive_link(get_post_type()); ?>">一覧に戻る</a></p>
-                        <p class="<?php if (get_next_post()) : ?>single_bottom_nav_next<?php endif; ?>"><?php next_post_link('%link', '次へ'); ?></p>
+                    <div class="c-single_bottom_nav">
+                        <p class="<?php if (get_previous_post()) : ?>c-single_bottom_nav__prev<?php endif; ?>">
+                            <?php 
+                            $prev_post = get_previous_post();
+                            if ($prev_post) {
+                                $prev_title = mb_strimwidth(get_the_title($prev_post->ID), 0, 20, '...');
+                                $prev_thumb = get_the_post_thumbnail($prev_post->ID, array(80, 80));
+                                $prev_link = get_permalink($prev_post->ID);
+                                echo '<span>前の記事</span>';
+                                echo '<a href="' . esc_url($prev_link) . '">&laquo; ' . $prev_thumb . $prev_title . '</a>';
+                            }
+                            ?>
+                        </p>
+                        <p class="c-single_bottom_nav__back">
+                            <a href="<?php echo get_post_type_archive_link(get_post_type()); ?>">一覧に戻る</a>
+                        </p>
+                        <p class="<?php if (get_next_post()) : ?>c-single_bottom_nav__next<?php endif; ?>">
+                            <?php 
+                            $next_post = get_next_post();
+                            if ($next_post) {
+                                $next_title = mb_strimwidth(get_the_title($next_post->ID), 0, 20, '...');
+                                $next_thumb = get_the_post_thumbnail($next_post->ID, array(80, 80));
+                                $next_link = get_permalink($next_post->ID);
+                                echo '<span>次の記事</span>';
+                                echo '<a href="' . esc_url($next_link) . '">' . $next_thumb . $next_title . ' &raquo;</a>';
+                            }
+                            ?>
+                        </p>
                     </div>
             </div>
 
