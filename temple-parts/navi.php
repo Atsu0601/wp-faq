a25cc08fa6df44ae609849584a55de72abd8b410
Kawatani Atsushi <katsushi61@gmail.com>
2023-12-31 07:36:07 +0900

    重要なお知らせの表示

--- temple-parts/navi.php
+++ temple-parts/navi.php
@@ -1,34 +1,40 @@
 <header class="l-header">
     <div class="l-inner">
 
-    <div class="c-news_important">
-        <div class="c-news_important__inner">
-            <ul class="c-news_important__blk">
-                <?php
-                $args = array(
-                    'post_type' => 'news',
-                    'posts_per_page' => 1,
-                    'meta_key' => 'important',
-                    'meta_value' => '1'
-                );
-                $the_query = new WP_Query($args);
-                if ($the_query->have_posts()) :
-                    while ($the_query->have_posts()) : $the_query->the_post();
-                ?>
-                        <li class="c-news_important__blk__list">
-                            <a href="<?php the_permalink(); ?>">
-                                <time datetime="<?php the_time('Y/m/d'); ?>"><?php the_time('Y/m/d'); ?></time>
-                                <span class="c-news_important__blk__list__txt"><?php the_title(); ?></span>
-                            </a>
-                        </li>
-                <?php
-                    endwhile;
-                endif;
-                wp_reset_postdata();
-                ?>
-            </ul>
+        <div class="c-news_important">
+            <div class="c-news_important__inner">
+                <ul class="c-news_important__blk">
+                    <?php
+                    $args = array(
+                        'post_type' => 'news',
+                        'posts_per_page' => 1,
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'news_cate',
+                                'terms' => array('important'),
+                                'field' => 'slug'
+                            ),
+                        ),
+                    );
+                    $the_query = new WP_Query($args);
+                    if ($the_query->have_posts()) :
+                        while ($the_query->have_posts()) : $the_query->the_post();
+                    ?>
+                            <li class="c-news_important__blk__list">
+                                <a href="<?php the_permalink(); ?>">
+                                    <time datetime="<?php the_time('c'); ?>"><?php the_time('Y/m/d'); ?></time>
+                                    <span class="c-news_important__blk__list__txt"><?php the_title(); ?></span>
+                                </a>
+                            </li>
+                    <?php
+                        endwhile;
+                    endif;
+                    wp_reset_postdata();
+                    ?>
+
+                </ul>
+            </div>
         </div>
-    </div>
 
         <div class="l-header__inner">
             <div class="l-header__logo">
