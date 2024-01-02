522008355a61aafeb83bda10b03be2b0bb4c2bb1
Kawatani Atsushi <katsushi61@gmail.com>
2024-01-03 08:28:37 +0900

    検索条件の調整

--- search.php
+++ search.php
@@ -7,16 +7,12 @@
         <div class="l-column2">
 
             <?php
-            // 検索ページテンプレートファイルの開始
-
-            get_header(); // WordPressのヘッダーを取得
-
             // 検索キーワードを安全に取得
             $search_keyword = isset($_GET['s']) ? esc_attr($_GET['s']) : '';
 
             // カスタムクエリの引数を設定
             $args = array(
-                'post_type'      => 'post', 'faq', // または 'page'、カスタム投稿タイプ名
+                'post_type'      => array('post', 'faq'), // または 'page'、カスタム投稿タイプ名
                 'posts_per_page' => 10,     // 1ページに表示する投稿数
                 's'              => $search_keyword, // 検索キーワード
                 'paged'          => get_query_var('paged') ? get_query_var('paged') : 1, // ページネーション
@@ -44,7 +40,7 @@
                 <?php if ($custom_query->have_posts()) : ?>
                     <p class="p-search__result_ttl">“<?php echo $search_keyword; ?>”の検索結果：<?php echo $custom_query->found_posts; ?>件</p>
                     <ul class="p-archive__blk">
-                        <?php while (have_posts()) : the_post(); ?>
+                    <?php while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
                             <li class="p-archive__blk__list">
                                 <a href="<?php the_permalink(); ?>">
                                     <div class="p-archive__blk__list__thumbnail">
@@ -72,14 +68,6 @@
                                         ?>
                                     </h3>
                                     <time class="p-archive__blk__list__date"><?php the_time("Y/m/d"); ?></time>
-                                    <!-- <div class="">
-                                    <?php
-                                    $terms = get_the_terms($post->ID, 'blog-cat');
-                                    foreach ($terms as $term) {
-                                        echo '<span>' . $term->name . '</span>';
-                                    }
-                                    ?>
-                                </div> -->
                                     <p class="p-archive__blk__list__txt">
                                         <?php
                                         if (mb_strlen($post->post_content, 'UTF-8') > 55) {
@@ -92,7 +80,7 @@
                                     </p>
                                 </a>
                             </li>
-                        <?php endwhile; ?>
+                            <?php endwhile; ?>
                     </ul>
                 <?php else : ?>
                     <p class="p-search__result_ttl">検索されたキーワードに該当する記事はありませんでした</p>
